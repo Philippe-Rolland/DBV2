856ea8336c60aa0db658f5e059f73953f59c583d
Philippe-Rolland <phetflo.rolland@orange.fr>
2017-07-12 15:55:37 +0200

    Add files via upload

---
+++ application/index.php
@@ -0,0 +1,39 @@
+<?php
+
+?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" lang="fr">
+
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<link rel="stylesheet" href="./css/style.css" type="text/css" media="screen" />
+</head>
+
+<body>
+	
+	<h1>Essai de codage des user/perso</h1>
+	
+	
+	<div class="UserPart">
+		<H2> Codage de la partie Users</H2>
+		
+		<form method="post" action="index.php">
+			<label>
+            Adresse e-mail :
+	            <input class="input-text" type="text" name="user_AdrMail"/>
+	        </label><br />
+	        <label>
+            Mot de passe :
+	            <input class="input-text" type="pasword" name="user_MDP"/>
+	        </label><br />
+	        
+	        <input class="input-submit" type=" button" value="Envoyer et continuer" />
+		</form>
+		
+	
+	</div>
+</body>
+
+</html>
